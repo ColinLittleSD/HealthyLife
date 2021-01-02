b82b33a4735f289677d2efa846bf64cb780002f7
ColinLittleSD <ColinLittleSD@gmail.com>
2021-01-02 13:40:06 -0500

    person list item

---
+++ HappyLife.Models/PersonListItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyLife.Models
+{
+    public class PersonListItem
+    {
+        [Key]
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        [Display(Name="Health Goals")]
+        public string HealthGoals { get; set; }
+        [Display(Name ="Date")]
+        public DateTime DateStarted { get; set; }
+    }
+}
---
+++ HealthyLife.WebMVC/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using HappyLife.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthyLife.WebMVC.Controllers
+{
+    [Authorize]
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            var model = new PersonListItem[0];
+            return View(model);
+        }
+    }
+}
