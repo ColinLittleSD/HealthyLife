17c25ec59dacabf129406f5acbd11d324d51f232
ColinLittleSD <ColinLittleSD@gmail.com>
2021-01-02 13:29:14 -0500

    PersonData

--- HappyLife.Data/Person.cs
+++ HappyLife.Data/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace HappyLife.Data
 {
     public class Person
     {
+        [Key]
+        public int PersonId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Weight { get; set; }
+        [Required]
+        [Display(Name="Health Goals")]
+        public string HealthGoals { get; set; }
+        [Required]
+        [Display(Name="Date")]
+        public DateTime DateStarted { get; set; }
     }
 }
