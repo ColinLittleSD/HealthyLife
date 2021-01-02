e110cb89acf3fbd8ebbde0af192b79605de31cdd
ColinLittleSD <ColinLittleSD@gmail.com>
2021-01-02 13:09:25 -0500

    Add project files.

---
+++ HealthyLife.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HealthyLife.WebMVC.MvcApplication" Language="C#" %>
---
+++ HealthyLife.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HealthyLife.WebMVC.Startup))]
+namespace HealthyLife.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
