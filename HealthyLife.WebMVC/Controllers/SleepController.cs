3e230a014fa052a8f25386378667c50d99a79467
ColinLittleSD <ColinLittleSD@gmail.com>
2021-01-03 21:14:45 -0500

    controller

--- HealthyLife.WebMVC/Controllers/SleepController.cs
+++ HealthyLife.WebMVC/Controllers/SleepController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HappyLife.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,14 @@ using System.Web.Mvc;
 
 namespace HealthyLife.WebMVC.Controllers
 {
+    [Authorize]
     public class SleepController : Controller
     {
         // GET: Sleep
         public ActionResult Index()
         {
-            return View();
+            var model = new SleepListItem[0];
+            return View(model);
         }
     }
 }
