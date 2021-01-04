23dd0181a6f6818f40a6175448c16d2481d80c97
ColinLittleSD <ColinLittleSD@gmail.com>
2021-01-04 11:52:09 -0500

    validation message

--- HealthyLife.WebMVC/Controllers/SleepController.cs
+++ HealthyLife.WebMVC/Controllers/SleepController.cs
@@ -31,17 +31,26 @@ namespace HealthyLife.WebMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(SleepCreate model)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
+            if (!ModelState.IsValid) return View(model);
 
+            var service = CreateSleepService();
+
+            if (service.CreateSleep(model))
+            {
+                TempData["SaveResult"] = "Your entry was created.";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Entry could not be created.");
+
+            return View(model);
+        }
+
+        private SleepService CreateSleepService()
+        {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new SleepService(userId);
-
-            service.CreateSleep(model);
-
-            return RedirectToAction("Index");
+            return service;
         }
     }
 }
