e95b6567b637f69b84624a53e5da1ed90351b62a
ColinLittleSD <ColinLittleSD@gmail.com>
2021-01-04 14:49:39 -0500

    exercise data

---
+++ HappyLife.Data/Exercise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyLife.Data
+{
+    public class Exercise
+    {
+        [Key]
+        public int ExerciseId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string Activity { get; set; }
+        [Required]
+        public double TimeSpentOnActivity { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+
+        public int PersonId { get; set; }
+        [ForeignKey(nameof(PersonId))]
+        public virtual Person Person { get; set; }
+    }
+}
---
+++ HealthyLife.WebMVC/Controllers/ExerciseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthyLife.WebMVC.Controllers
+{
+    public class ExerciseController : Controller
+    {
+        // GET: Exercise
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
