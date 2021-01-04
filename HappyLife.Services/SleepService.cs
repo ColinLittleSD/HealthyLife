6f68da28ec1eaaaaa206f25e8a62f91d833bfefc
ColinLittleSD <ColinLittleSD@gmail.com>
2021-01-04 10:56:44 -0500

    sleep service

---
+++ HappyLife.Services/SleepService.cs
@@ -0,0 +1,63 @@
+﻿using HappyLife.Data;
+using HappyLife.Models;
+using HealthyLife.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyLife.Services
+{
+    public class SleepService
+    {
+        private readonly Guid _userId;
+
+        public SleepService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateSleep(SleepCreate model)
+        {
+            var entity =
+                new Sleep()
+                {
+                    OwnerId = _userId,
+                    HoursSlept = model.HoursSlept,
+                    WakeUpTime = model.WakeUpTime,
+                    Date = model.Date
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Sleeps.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<SleepListItem> GetSleeps()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Sleeps
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new SleepListItem
+                                {
+                                    SleepId = e.SleepId,
+                                    HoursSlept = e.HoursSlept,
+                                    WakeUpTime = e.WakeUpTime,
+                                    Date = e.Date
+                                    
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
