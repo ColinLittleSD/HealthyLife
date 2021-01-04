18a882de26128763f237afc649aff8dfc46018d7
ColinLittleSD <ColinLittleSD@gmail.com>
2021-01-03 20:49:40 -0500

    Sleep Data/controller start/listitem

---
+++ HappyLife.Models/SleepListItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyLife.Models
+{
+    public class SleepListItem
+    {
+        [Key]
+        public int SleepId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        [Display(Name = "How long did you sleep?")]
+        public int HoursSlept { get; set; }
+        [Required]
+        [Display(Name = "Time woken up")]
+        public TimeSpan WakeUpTime { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ HealthyLife.WebMVC/Controllers/SleepController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthyLife.WebMVC.Controllers
+{
+    public class SleepController : Controller
+    {
+        // GET: Sleep
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
