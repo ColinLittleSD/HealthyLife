593e77c0ce480cca717aa35a9cbaed6a5a3f413a
ColinLittleSD <ColinLittleSD@gmail.com>
2021-01-10 15:56:22 -0500

    happiness create

--- HealthyLife.WebMVC/Controllers/HappinessController.cs
+++ HealthyLife.WebMVC/Controllers/HappinessController.cs
@@ -1,4 +1,5 @@
 ﻿using HappyLife.Models;
+using HappyLife.Models.happinessmodels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,22 @@ namespace HealthyLife.WebMVC.Controllers
         public ActionResult Index()
         {
             var model = new HappinessListItem[0];
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(HappinessCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
             return View(model);
         }
     }
